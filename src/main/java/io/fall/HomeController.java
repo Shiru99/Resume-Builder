2c5b0030937ad41d230731e24d18341f72474973
Shiru99 <shriram.ghadge1999@gmail.com>
2021-07-19 11:49:19 +0530

    thymeleaf : HTML profile added

--- src/main/java/io/fall/HomeController.java
+++ src/main/java/io/fall/HomeController.java
@@ -1,9 +1,14 @@
 package io.fall;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+// @RestController // rest controller - JSON only
+@Controller
 public class HomeController {
     
     @RequestMapping("/")
@@ -14,5 +19,11 @@ public class HomeController {
     @RequestMapping("/edit")
     public String sayWelcomeAdmin() {
         return "<h1>Welcome to Resume-Builder, Resume-Owner</h1>";
+    }
+
+    @GetMapping("/view/{userId}")
+    public String viewProfile(@PathVariable String userId, Model model){
+        model.addAttribute("userId", userId);
+        return "profile";
     }
 }
